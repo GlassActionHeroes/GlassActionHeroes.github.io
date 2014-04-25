eeeafe836df2f30b83ba8c9e49740156ede2b0aa
Chae O'Keefe <chaemobile@gmail.com>
2014-04-25 19:01:21 -0400

    jsonp . this is amazing

--- js/pusher.js
+++ js/pusher.js
@@ -1,5 +1,10 @@
-var pusher = new Pusher('514e04bbf50ba9b0b0b6');
-var channel = pusher.subscribe('test_channel');
-channel.bind('my_event', function(data) {
-  alert(data);
+var pusher = new Pusher('514e04bbf50ba9b0b0b6', {
+    authTransport: 'jsonp',
+    authEndpoint: 'http://mysterious-forest-1989.herokuapp.com/pusher/auth'
+});
+
+var channel = pusher.subscribe('private-bnr_2048_channel');
+
+channel.bind('client-send_direction', function(data) {
+  alert(data.up);
 });
