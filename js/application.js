47adef0b3bbd4b41d60aead5762d0567124c861a
Chae O'Keefe <chaemobile@gmail.com>
2014-04-25 12:01:47 -0400

    Amazing nerd gifs 1-3

--- js/application.js
+++ js/application.js
@@ -10,10 +10,10 @@ window.requestAnimationFrame(function () {
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-gradient-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-hat-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-peepers-212.gif",
-        "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-prizza-212.gif",        
+        "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-prizza-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-rainbow-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-shake-space-212.gif",
-        "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-sunglasses-212.gif",        
+        "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-sunglasses-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-shake-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/212/doge-wink-212.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-derp-114.gif",
@@ -21,14 +21,14 @@ window.requestAnimationFrame(function () {
         "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-gradient-114.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-hat-114.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-peepers-114.gif",
-        "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-prizza-114.gif",        
+        "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-prizza-114.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-rainbow-114.gif",
         "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-shake-space-114.gif",
-        "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-sunglasses-114.gif",        
-        "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-shake-114.gif",
-        "http://d1e3ezyatlol8u.cloudfront.net/img/114/doge-wink-114.gif",
+        "../image/212/3.gif",
+        "../image/212/2.gif",
+        "../image/212/1.gif",
     ];
-    for(var i = 0; i < imageList.length; i++ ) 
+    for(var i = 0; i < imageList.length; i++ )
     {
         var imageObject = new Image();
         imageObject.src = imageList[i];
